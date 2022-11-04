924b4dc82036fb83e5c3f9f633d8464cbdce59dd
tuhin902 <tuhin902md@gmail.com>
2022-11-04 20:30:30 +0600

    log in added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -3,20 +3,21 @@ import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
-const Login = () => {
+const Signup = () => {
 
-    const { login } = useContext(AuthContext);
+    const { createUser } = useContext(AuthContext);
 
-    const handleLogin = event => {
+    const handleSignup = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
 
-        login(email, password)
+        createUser(email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
+
             })
             .catch(error => console.error(error));
 
@@ -29,32 +30,35 @@ const Login = () => {
                     <img className='w-3/4' src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <h1 className="text-5xl text-center font-bold">Login!</h1>
+                    <form onSubmit={handleSignup} className="card-body">
+                        <h1 className="text-5xl text-center font-bold">Sign up</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
                         </div>
                         <div className="form-control mt-6">
-                            <input className="btn btn-primary" type="submit" value="Login" />
+                            <input className="btn btn-primary" type="submit" value="Sign up" />
                         </div>
                     </form>
-                    <p className='text-center'>New to genius car <Link className='text-orange-600 font-bold' to='/signup'>Sign up</Link> </p>
+                    <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to='/login'>Log in</Link> </p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Signup;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
             }
         ]
     }
